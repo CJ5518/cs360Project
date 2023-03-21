2eff19a30a0bf4c4da5fd6c1799e5e36131f6911
Carson Rueber <carsonrueber@gmail.com>
2023-03-21 09:18:38 -0700

    Service box clickable

--- widgets/serviceBox.lua
+++ widgets/serviceBox.lua
@@ -2,6 +2,8 @@ local Widget = require("lapis.html").Widget
 
 local servBox, servBox_mt = Widget:extend("servBox")
 
+--In the future, we'd want this to only take in the service Id and then do some sort of database query
+
 local items = {
 	{"Type: ", "serviceType"},
 	{"Price: ", "servicePrice"},
@@ -9,7 +11,7 @@ local items = {
 }
 
 function servBox_mt:content()
-	div({class = "service-box"}, function()
+	div({class = "service-box", onclick="location.href='service/" .. (math.floor(math.random() * 1000) .. "'")}, function()
 		ul({class = "service-box-list"}, function ()
 			li(function() 
 				span({class = "service-box-title"}, self.title)
