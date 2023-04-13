e30e29ee246f6987838d2a9c02c87bd1c1de044b
Carson Rueber <carsonrueber@gmail.com>
2023-04-12 23:49:13 -0700

    Basic homes, no interface yet

--- apps/api.lua
+++ apps/api.lua
@@ -5,6 +5,8 @@ local db = require("lapis.db");
 local send_email = require("helpers.email").send_email;
 local Users = require("models.Users");
 local Vendors = require("models.Vendors");
+local Homes = require("models.Homes");
+local homesHelper = require("helpers.homes");
 local accounts = require("helpers.accounts");
 local getRandomString = require("helpers.randomString").getRandomString;
 
@@ -64,8 +66,12 @@ end)
 
 --Log out
 
-app:match("logout", "/logout", function(self)
+app:post("logout", "/logout", function(self)
 	accounts.logOut(self);
+end)
+app:get("logout", "/logout", function(self)
+	accounts.logOut(self);
+	return {redirect_to = "index"};
 end)
 
 
@@ -73,6 +79,7 @@ end)
 
 app:post("passwordResetRequest", "/passwordResetRequest", function(self)
 	local email = self.POST.email;
+	--TODO (vendor support)
 	local user = Users:find({["email"] = email});
 	if user then
 		--TODO (Maybe) (I think this was (adjust dumbness))
@@ -115,6 +122,80 @@ app:post("updateUserInfoAction", "/updateUserInfoAction", function(self)
 	else
 		self:write("Something went wrong, sorry! (updateUserInfoAction)");
 	end
+end)
+
+
+--Update vendor info
+--TODO (Make it work for vendors, file uploads)
+--Very broken try not to use
+app:post("updateVendorInfoAction", "/updateVendorInfoAction", function(self)
+	local user = accounts.isLoggedIn(self);
+	if user then
+		user.CompanyName = self.POST.FirstName == "" and user.FirstName or self.POST.FirstName;
+		user.LastName = self.POST.LastName == "" and user.LastName or self.POST.LastName;
+		user.PhoneNumber = self.POST.PhoneNumber == "" and user.PhoneNumber or self.POST.PhoneNumber;
+		user.AnnualIncome = self.POST.AnnualIncome == "" and user.AnnualIncome or self.POST.AnnualIncome;
+		user:update("FirstName", "LastName", "PhoneNumber", "AnnualIncome");
+		return {redirect_to = "/dashboard"};
+	else
+		self:write("Something went wrong, sorry! (updateVendorInfoAction)");
+	end
+end)
+
+--All to do with creating and editing homes
+
+--Accepts POST data in the form
+--self.POST.Address = "123 Fake Street";
+--self.POST.NewHome = true; --IF you want to make a new home
+app:post("homes", "homes/", function(self)
+	--TODO (finish)
+	local data = self.POST;
+
+	--Does this home exist?
+
+	--Bad update request
+	if (not data.HomeID or data.HomeID < 0) and not data.NewHome then
+		return {status = 400};
+	end
+
+	--Only users get to add homes
+	if not (accounts.getAccountType(self) == "User") then
+		return {status = 401};
+	end
+
+	local home = Homes:find(data.HomeID);
+	--Editing home but couldn't find the given id
+	if not home and not data.NewHome then
+		return {status = 404};
+	end
+
+	--Get the data from post into something concrete that we know is good
+	local updateTable = {};
+	for q = 1, #homesHelper.fieldsWithoutIDs do
+		local name, type = homesHelper.fieldsWithoutIDs[q][1], homesHelper.fieldsWithoutIDs[q][2];
+		local newFieldValue;
+		if type == "int" then
+			newFieldValue = tonumber(data[name]);
+		elseif type == "string" then
+			--It probably already is a string but whatever
+			newFieldValue = tostring(data[name]);
+		else
+			error("Can't handle a type from homesHelper.fieldsWithoutIDs");
+		end
+		updateTable[name] = newFieldValue;
+	end
+	--If just updating
+	if not data.NewHome then
+		home:update(updateTable);
+	else --Making new home
+		updateTable["HomeOwner"] = self.account.UserID; --We make sure we are logged in as user beforehand
+		Homes:create(updateTable);
+	end
+end)
+
+app:delete("homes", "homes/", function(self)
+	--TODO
+
 end)
 
 return app;
