bb344c8efca9a2d0e31cb290a24cffa5b444a753
Carson Rueber <carsonrueber@gmail.com>
2023-04-24 17:23:56 -0700

    service page works

--- models/Services.lua
+++ models/Services.lua
@@ -1,7 +1,17 @@
 local Model = require("lapis.db.model").Model;
 local db = require("lapis.db");
+local servicesHelper = require("helpers.services");
+
 local Services, Services_mt = Model:extend("Services", {
 	primary_key = "ServiceID"
 });
+
+function Services_mt:getTypeFields()
+	return servicesHelper.typeFields[servicesHelper.types[self.ServiceTypeID]];
+end
+
+function Services_mt:getField(q)
+	return self["Field" .. tostring(q)]
+end
 
 return Services;
