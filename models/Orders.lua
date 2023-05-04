4c31927a627410a233ef099455dbbbace70219ff
Carson Rueber <carsonrueber@gmail.com>
2023-05-04 01:34:57 -0700

    Orders work

--- models/Orders.lua
+++ models/Orders.lua
@@ -6,21 +6,31 @@ local Services = require("models.Services");
 local Users = require("models.Users");
 local Vendors = require("models.Vendors");
 
-local Orders, Orders_mt = Model:extend("Services", {
-	primary_key = "ServiceID"
+local Orders, Orders_mt = Model:extend("Orders", {
+	primary_key = "OrderID"
 });
 
 function Orders_mt:getHome()
 	return Homes:find(self.HomeID)
 end
 function Orders_mt:getUser()
-	return Homes:find(self.UserID)
+	return Users:find(self.UserID)
 end
 function Orders_mt:getVendor()
-	return Homes:find(self.VendorID)
+	return Vendors:find(self.VendorID)
 end
 function Orders_mt:getService()
-	return Homes:find(self.ServiceID)
+	return Services:find(self.ServiceID)
+end
+function Oders_mt:getState()
+	return self.OrderStateEnum;
+end
+function Orders:getStateEnumName(id)
+	local tab = {
+		"Requested",
+		"Accepted"
+	}
+	return tab[id];
 end
 
 return Orders;
---
+++ widgets/orderBox.lua
@@ -0,0 +1,53 @@
+local Widget = require("lapis.html").Widget
+
+local Orders = require("models.Orders");
+local orderBox, orderBox_mt = Widget:extend("orderBox")
+local accounts = require("helpers.accounts");
+
+--NEEDS OrderID
+
+function orderBox_mt:content()
+	div({class = "service-box"}, function()
+		ul({class = "service-box-list"}, function ()
+			if not self.OrderID then
+				text("orderBox.lua: no OrderID passed");
+			else
+				local order = Orders:find(self.OrderID);
+				
+				li(function() 
+					span({class = "service-box-title service-box-item", 
+					onclick="location.href='order/" .. tostring(self.OrderID) .. "'"}, "Order for: " .. order:getService().Name)
+				end)
+				--noClick must be set on button fields
+				local function field(str, noClick)
+					li(function ()
+						local onclick="location.href='order/" .. tostring(self.OrderID) .. "'";
+						if noClick then
+							onclick = false;
+						end
+						span({class = "service-box-item", ["onclick"] = onclick}, str);
+					end)
+				end
+				field("From: " .. order:getVendor().CompanyName);
+				local canSee = false;
+				if self.account then
+					if accounts.getAccountType(self) == "User" then
+						if order:getUser().UserID == accounts.isLoggedIn(self).UserID then
+							canSee = true;
+						end
+					end
+					if accounts.getAccountType(self) == "Vendor" then
+						if order:getUser().VendorID == accounts.isLoggedIn(self).VendorID then
+							canSee = true;
+						end
+					end
+				end
+				if self.account and canSee then
+					field("Status: " .. Orders:getStateEnumName(order.OrderStateEnum));
+				end
+			end
+		end)
+	end)
+end
+
+return orderBox
